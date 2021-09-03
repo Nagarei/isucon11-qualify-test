ac4c28ecce6a8dbe1ec7ce2f0d233b3ad1fe5a1a
Nagarei <Nagarei@users.noreply.github.com>
2021-09-03 23:03:44 +0900

    クエリに limit をつける (GET /api/trend)

--- go/main.go
+++ go/main.go
@@ -1108,7 +1108,7 @@ func getTrend(c echo.Context) error {
 		for _, isu := range isuList {
 			conditions := []IsuCondition{}
 			err = db.Select(&conditions,
-				"SELECT * FROM `isu_condition` WHERE `jia_isu_uuid` = ? ORDER BY timestamp DESC",
+				"SELECT * FROM `isu_condition` WHERE `jia_isu_uuid` = ? ORDER BY timestamp DESC LIMIT 1",
 				isu.JIAIsuUUID,
 			)
 			if err != nil {
